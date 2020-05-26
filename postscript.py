c210c8006c1a1e0a1fea84702fb838a3e5362f9d
Penny Liu <penny.runzeliu@gmail.com>
2020-05-25 22:12:53 -0400

    cleaned skeleton code

--- postscript.py
+++ postscript.py
@@ -1,13 +1,4 @@
-# WRITE YOUR NAME and YOUR COLLABORATORS HERE
-
-#------------------------- 10% -------------------------------------
-# The operand stack: define the operand stack and its operations
 opstack = []  #assuming top of the stack is the end of the list
-
-# Now define the HELPER FUNCTIONS to push and pop values on the opstack 
-# Remember that there is a Postscript operator called "pop" so we choose 
-# different names for these functions.
-# Recall that `pass` in python is a no-op: replace it with your code.
 
 def opPop():
     if opstack==[]:
@@ -19,8 +10,6 @@ def opPop():
 def opPush(value):
     opstack.append(value)
 
-#-------------------------- 20% -------------------------------------
-# The dictionary stack: define the dictionary stack and its operations
 dictstack = []  #assuming top of the stack is the end of the list
 
 # now define functions to push and pop dictionaries on the dictstack, to 
@@ -64,10 +53,6 @@ def lookup(name):
     # What is your design decision about what to do when there is no definition for “name”? If “name” is not defined, your program should not break, but should give an appropriate error message.
 
 
-#--------------------------- 10% -------------------------------------
-# Arithmetic, comparison, and boolean operators: add, sub, mul, eq, lt, gt, and, or, not 
-# Make sure to check the operand stack has the correct number of parameters 
-# and types of the parameters are correct.
 def add():
     if len(opstack) > 1:
         op2 = opPop()
@@ -390,11 +375,6 @@ def counttomark():
 def stack():
     print(opstack)
 
-#--------------------------- 20% -------------------------------------
-# Define the dictionary manipulation operators: psDict, begin, end, psDef
-# name the function for the def operator psDef because def is reserved in Python. Similarly, call the function for dict operator as psDict.
-# Note: The psDef operator will pop the value and name from the opstack and call your own "define" operator (pass those values as parameters).
-# Note that psDef()won't have any parameters.
 
 def psDict():
     size = opPop()
